a459c3d8595482d4ecf3866a72f454a9dc4dd526
VladyslavRiaboshapko <krentrow@gmail.com>
2025-09-14 13:58:26 +0300

    Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string num_s = Console.ReadLine();
+            bool num = int.TryParse(num_s, out int number);
+
+            if (num == true)
+            {
+                if (number % 10 == 0)
+                {
+                    Console.WriteLine("Multiple of 10");
+                }
+                else
+                {
+                    Console.WriteLine("Not a multiple of 10");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input");
+            }
+        }
+    }
+}
